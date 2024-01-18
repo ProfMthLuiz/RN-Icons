7a8ed7b906491dde92d478db51627bf4a5459868
Matheus <mthluiz99@gmail.com>
2024-01-18 13:03:47 -0300

    adding first project

---
+++ App.js
@@ -0,0 +1,22 @@
+// node_modules/@expo/vector-icons
+import { StyleSheet, Text, View } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <FontAwesome size={100} color="red" name="facebook" />
+      <Text style={{ fontSize: 50 }}>acebook</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
