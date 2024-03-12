0d4e7aff5025b6607f2223b61484c8e94cca7aa7
ProfMatheus <154000567+ProfMthLuiz@users.noreply.github.com>
2024-03-12 15:24:37 -0300

    Update

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-// https://icons.expo.fyi/Index
 // node_modules/@expo/vector-icons
 import { StyleSheet, Text, View } from "react-native";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <FontAwesome size={100} color="red" name="facebook" />
-      <Text style={{ fontSize: 50 }}>facebook</Text>
+      <FontAwesome size={50} color="brown" name="envelope" />
+      <FontAwesome size={50} color="yellow" name="eye" />
+      <AntDesign name="stepforward" size={50} color="black" />
+      <Ionicons name="md-checkmark-circle" size={50} color="green" />
+      <FontAwesome size={50} color="red" name="facebook" />
+      <Text style={{ fontSize: 50 }}>acebook</Text>
     </View>
   );
 }
